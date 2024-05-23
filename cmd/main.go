4bebcd8f5cc6ac9271186f89e6d610c967888978
Al Fi <a.figilyantov@mail.ru>
2024-05-23 17:57:23 +0500

    first version of CLI tool

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func commandCleaning(c string) string {
+
+	return strings.ToLower(strings.TrimSpace(c))
+
+}
+
 func main() {
+	file := flag.String("file", "", "name of file or full path for command")
+	command := flag.String("do", "", "command for file treatment: create, read, delete")
+
+	flag.Parse()
+
+	var emptyValue string
+
+	if *file == emptyValue {
+		log.Fatal("Do not exist file name or file path. Tool is clousing")
+	}
+
+	if *command == emptyValue {
+		log.Fatal("Command is absent. Try one more time from the begining. Application is shooting down.")
+	}
+
+	cleanedCommand := commandCleaning(*command)
+
+	switch cleanedCommand {
+
+	case emptyValue:
+		log.Fatal("Command is absent. Try one more time from the begining. Application is shooting down.")
+
+	case "create":
+		create(*file)
+
+	case "read":
+		read(*file)
+
+	case "delete":
+		remove(*file)
+
+	default:
+		fmt.Printf("Unknow command %s. Start application with flag -help. Read manual", cleanedCommand)
+	}
 
 }
